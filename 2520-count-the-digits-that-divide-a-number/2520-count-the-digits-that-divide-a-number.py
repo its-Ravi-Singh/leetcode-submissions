d8f4f0b17f7633c146fa08dc975316ca151cc195
Ravi Rajaram Singh <86908196+its-Ravi-Singh@users.noreply.github.com>
2026-08-28 17:09:34 -0400

    Time: 0 ms (100%), Space: 19.3 MB (59.03%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -5,8 +5,6 @@ class Solution:
         num_check = num
         while num > 0:
             if num_check % (num % 10) == 0:
-                # print(num % 10)
                 val += 1
             num //= 10
-        print(val)
         return val
