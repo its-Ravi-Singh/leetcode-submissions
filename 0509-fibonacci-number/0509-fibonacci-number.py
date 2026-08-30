b5d9d752a897391716b82299b26634347d39111a
Ravi Rajaram Singh <86908196+its-Ravi-Singh@users.noreply.github.com>
2026-08-30 19:07:11 -0400

    Time: 35 ms (97.51%), Space: 19.4 MB (19.4%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,14 @@
 class Solution:
-    def fib(self, n: int) -> int:
-        # Recursion Solution
+    def fib(self, n: int, dp: list = []) -> int:
+        # DP Memoization Method
+        if not dp:
+            dp = [-1 for i in range(n+1)]
+        
         if n <= 1:
             return n
-        return self.fib(n-1) + self.fib(n-2)
+
+        if dp[n] != -1:
+            return dp[n]
+        
+        dp[n] = self.fib(n-1, dp) + self.fib(n-2, dp)
+        return dp[n]
