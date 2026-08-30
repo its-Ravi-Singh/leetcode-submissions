2a191df02302d0b6e8ab65c6675014ee46c0334a
Ravi Rajaram Singh <86908196+its-Ravi-Singh@users.noreply.github.com>
2026-08-30 19:17:57 -0400

    Time: 50 ms (51.36%), Space: 19.1 MB (57.7%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,7 @@
 class Solution:
     def fib(self, n: int, prev1: int = 1, prev2: int = 0) -> int:
         # DP Memoization Method more optimise with only 2 var
-        if n <= 1:
-            return n
+        if n <= 1: return n
         for i in range(2, n+1):
-            lru_cache(None)
-            temp = prev1 + prev2
-            prev1, prev2 = temp, prev1
+            prev1, prev2 = prev1 + prev2, prev1
         return prev1
