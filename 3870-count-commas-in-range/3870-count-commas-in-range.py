79a2f6b48840f48cf26a6c2dc4f4a3d76ef676e7
Ravi Rajaram Singh <86908196+its-Ravi-Singh@users.noreply.github.com>
2026-09-07 23:32:40 -0400

    Time: 0 ms (100%), Space: 19.2 MB (86.58%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -1,5 +1,4 @@
 import math
 class Solution:
     def countCommas(self, n: int) -> int:
-        print(math.log10(n))
-        return n-999 if math.log10(n) >= 3 else 0
+        return max(n-999, 0)
